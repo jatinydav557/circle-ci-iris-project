16d5cf36318364590721c9bd06483da7b4fab90c
Jatin <jatinydav557@gmail.com>
2025-07-02 09:32:01 +0530

    test

--- pipeline/training_pipeline.py
+++ pipeline/training_pipeline.py
@@ -1,6 +1,8 @@
+import sys
+import os
 from src.data_processing import DataProcessing
 from src.model_training import ModelTraining
-
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 if __name__ == "__main__":
     data_processor = DataProcessing("artifacts/raw/data.csv")
     data_processor.run()
